f698f5dc07bacd310c1749d08943b8b7850f3a0c
youmna ahmed <you@example.com>
2022-09-30 22:53:29 +0200

    third commit

--- ash_allSquad_bluetoothCar.ino
+++ ash_allSquad_bluetoothCar.ino
@@ -1,9 +1,6 @@
-//#include <SoftwareSerial.h>
-//SoftwareSerial mySerial(12,13);
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(12,13); //instead of tx and rx in arduino
 
-// C++ code
-//#define trigPin A0
-//#define echoPin A1
 int trigPin = A0;    // Trigger
 int echoPin = A1;    // Echo
 long cm, inches;
@@ -17,8 +14,8 @@ char reading;
 long duration, distance;
 
 void setup()
-{Serial.begin(9600);
-mySerial.begin(9600);
+{Serial.begin(9600);//for serial monitor
+ mySerial.begin(9600);//for bluetooth connection
 
  pinMode(leftIn1,OUTPUT);
  pinMode(leftIn2,OUTPUT);
@@ -38,52 +35,49 @@ void loop()
     delayMicroseconds(10);
     digitalWrite(trigPin,LOW);
     duration=pulseIn(echoPin,HIGH);
-    cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;
-  Serial.print(inches);
-  Serial.print("in, ");
+    cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343 which is sound velocity
+
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
   
   delay(250);
-    distance =(duration/2)*0.0343;
    Serial.println(distance);
-  if(mySerial.available()>0)
+  if(mySerial.available()>0)//when bluetooth sends data
   {
     
 
     reading=mySerial.read();
-    switch(reading)
+    switch(reading)  //bluetooth app sends only one letter
     {
-      case 'F':
-         if(distance>30){
-        digitalWrite(leftIn1,HIGH);
+      case 'F'://F to move car forward
+         if(distance>30){       
+        digitalWrite(leftIn1,HIGH);        //both motors rotate counter clockwise
         digitalWrite(rightIn1,HIGH);
         digitalWrite(leftIn2,LOW);
         digitalWrite(rightIn2,LOW);
         analogWrite(enLeft,255);
         analogWrite(enRight,255);}
         break;
-      case 'B':
+      case 'B'://B to move car backward
         digitalWrite(leftIn1,LOW);
         digitalWrite(rightIn1,LOW);
-        digitalWrite(leftIn2,HIGH);
+        digitalWrite(leftIn2,HIGH);  //both motors rotate clockwise
         digitalWrite(rightIn2,HIGH);
         analogWrite(enLeft,255);
         analogWrite(enRight,255);
         break;
-      case 'L':
-        digitalWrite(leftIn1,LOW);
+      case 'L'://L to rotate car left
+        digitalWrite(leftIn1,LOW);    //right motor is on and the left motor is off
         digitalWrite(rightIn1,HIGH);
         digitalWrite(leftIn2,LOW);
         digitalWrite(rightIn2,LOW);
         analogWrite(enLeft,0);
         analogWrite(enRight,255);
          break;
-      case 'R':
+      case 'R'://R to rotate car right
         digitalWrite(leftIn1,HIGH);
-        digitalWrite(rightIn1,LOW);
+        digitalWrite(rightIn1,LOW);  //left motor is on and the right motor is off
         digitalWrite(leftIn2,LOW);
         digitalWrite(rightIn2,LOW);
         analogWrite(enLeft,255);
@@ -96,27 +90,5 @@ void loop()
         
     }
   }
-  
-  
- while(!digitalRead(forward)) //while button is low both motors rotate counter clockwise
- { delay(10);   // delay for debouncing
-//  
-//  
- }
- while(!digitalRead(backward))//while button is low both motors rotate clockwise
- { delay(10);
-
- }
-  while(!digitalRead(rightRotate))//while button is low,left motor is on and the right motor is off
- { delay(10);
-
- }
-  while(!digitalRead(leftRotate))//while button is low,right motor is on and the left motor is off
- { delay(10);
-
- }
-
-
- 
 
 }
